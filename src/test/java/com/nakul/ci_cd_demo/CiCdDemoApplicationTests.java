f0afefbd3865713b232063b1b7e55908694c4bef
NakulSambare <sambarenakul@gmail.com>
2026-05-31 13:26:58 +0530

    commit1

---
+++ src/test/java/com/nakul/ci_cd_demo/CiCdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nakul.ci_cd_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
